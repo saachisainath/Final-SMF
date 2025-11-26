9b0aeb05d591d1b14553a010a542432b2fb8e559
saachisainath <ss18735@nyu.edu>
2025-11-26 13:45:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-#import sklearn as sk
-#import seaborn as sns
-#import plotly.express as px
-#from sklearn.model_selection import train_test_split
-#from sklearn.preprocessing import LabelEncoder, StandardScaler
-#from sklearn.metrics import accuracy_score, r2_score, mean_absolute_error
-#import streamlit.components.v1 as components
-#import looker_sdk
+import sklearn as sk
+import seaborn as sns
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.metrics import accuracy_score, r2_score, mean_absolute_error
+import streamlit.components.v1 as components
+import looker_sdk
 
 st.sidebar.title("Happy Place")
 st.sidebar.write("Notice what you need. Nurture how you feel")
