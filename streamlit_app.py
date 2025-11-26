83bc77ef7a2fc7e9657d8d8d6b5c7c4a621b4bd4
saachisainath <ss18735@nyu.edu>
2025-11-26 14:32:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,46 @@ df = pd.read_csv("Mental_Health_and_Social_Media_Balance_Dataset.csv")
 page = st.sidebar.selectbox("Select Page",["Introduction","Data Viz","Prediction", "Crystal Ball",])
 ##Introduction Page
 if page == "Introduction":
+
+    st.image("hugol-halpingston-4OyLq2yN9u0-unsplash.jpg", use_container_width=True)
+    st.markdown("<h2 style='color: lightpink; text-align: center;'>Introduction</p>", unsafe_allow_html=True)
+
+
+    st.subheader("Our Mission")
+    st.write("Sometimes you don’t even know why you’re feeling low, only that something needs to change. This app helps you pause, reflect, and uncover the patterns beneath your mood by organizing your daily habits—sleep, social media use, movement, and more—into clear, meaningful insights. As you start to see how your choices shape your well-being, you realize you have far more control than you thought.")
+    st.markdown("<p style='color: lightpink; '>Take a moment, search inward, and let your data gently guide you toward a happier, more intentional life!</p>", unsafe_allow_html=True)
+
+
+    st.subheader("Look Inward")
+    
+    st.markdown("<p style='color: lightpink; '>Happiness is less complicated than you may think. Let's break down the factors that contribute to your mental health.</p>", unsafe_allow_html=True)
+
+
+    st.subheader("Data Set")
+
+    st.markdown("##### Data Preview")
+
+    rows = st.slider("Select a number of rows",5,20,5)
+    
+    st.dataframe(df.head(rows))
+
+    st.markdown("##### Missing Values")
+
+    missing = df.isnull().sum()
+    st.write(missing)
+
+    if missing.sum()==0:
+        st.success("No missing values found")
+    else:
+        st.warning("This data has some missing values")
+
+    st.markdown("#### Statistical Summary")
+
+    if st.button("Click Here to Generate Statistical Summary!"):
+        st.dataframe(df.describe())
+
+
+if page == "Data Viz":
     st.markdown("""
     <style>
     
@@ -76,41 +116,9 @@ if page == "Introduction":
         <span class="flower" style="left: 90%; animation-duration: 14s; animation-delay: 3s;">🌸</span>
     </div>
     """, unsafe_allow_html=True)
-    st.image("hugol-halpingston-4OyLq2yN9u0-unsplash.jpg", use_container_width=True)
-    st.markdown("<h2 style='color: lightpink; text-align: center;'>Introduction</p>", unsafe_allow_html=True)
 
-
-    st.subheader("Our Mission")
-    st.write("Sometimes you don’t even know why you’re feeling low, only that something needs to change. This app helps you pause, reflect, and uncover the patterns beneath your mood by organizing your daily habits—sleep, social media use, movement, and more—into clear, meaningful insights. As you start to see how your choices shape your well-being, you realize you have far more control than you thought.")
-    st.markdown("<p style='color: lightpink; '>Take a moment, search inward, and let your data gently guide you toward a happier, more intentional life!</p>", unsafe_allow_html=True)
-
-
-    st.subheader("Look Inward")
-    
-    st.markdown("<p style='color: lightpink; '>Happiness is less complicated than you may think. Let's break down the factors that contribute to your mental health.</p>", unsafe_allow_html=True)
-
-
-    st.subheader("Data Set")
-
-    st.markdown("##### Data Preview")
-
-    rows = st.slider("Select a number of rows",5,20,5)
-    
-    st.dataframe(df.head(rows))
-
-    st.markdown("##### Missing Values")
-
-    missing = df.isnull().sum()
-    st.write(missing)
-
-    if missing.sum()==0:
-        st.success("No missing values found")
-    else:
-        st.warning("This data has some missing values")
-
-    st.markdown("#### Statistical Summary")
-
-    if st.button("Click Here to Generate Statistical Summary!"):
-        st.dataframe(df.describe())
 ## Business Problem Presentation
+
+
+
 ## Data Summary Presentation
